1f051c315297bbec7cbb024ad1f9ab6691829ef6
Jeremiah Augustin <Phantom4iife7@gmail.com>
2025-03-18 01:02:43 -0400

    Finished Project

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace MethodOverloading
+﻿using System;
+
+namespace MethodOverloading
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            
+            Console.WriteLine(Sum(5, 10))              
+            
+            Console.WriteLine(Sum(5.5, 4.5));            
+            Console.WriteLine(FormattedSum(1, 0, true)); 
+            Console.WriteLine(FormattedSum(5, 11, true)); 
+          
+            Console.WriteLine(FormattedSum(0, 0, true)); 
+        }
+
+        
+        public static int Sum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        
+        public static double Sum(double num1,  double num2)
+        {
+            return num1 + num2;
+            
+        }
+
+        
+        public static string FormattedSum(int num1, int num2, bool useCurrency)
+        {
+            int total = num1 + num2;
+
+            
+            if (!useCurrency) return total.ToString();
+
+            return total == 1 ? total + " dollar" : total + " dollars";
         }
     }
 }
